d14452d15d00ce186cecf81f5f2911b3b3596f68
sandeep <ganavis027@gmail.com>
2026-04-17 14:27:47 +0530

    React-App

---
+++ todo app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Todoinput from "./components/Todoinput"
+
+function App(){
+  return (
+    <div>
+      <h1>Todo App</h1>c
+      <Header />
+      <Todoinput />
+    </div>
+  ) 
+}
+
+export default App
---
+++ todo app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+  return (
+    <header>
+        <h1> MY Todo App</h1>   
+    </header>
+    
+  ) 
+}                           
+
+export default Header;
